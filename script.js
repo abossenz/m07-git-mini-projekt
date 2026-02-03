31484146b6ad6bf8689593487faa1047240c44a3
Matthias Kühne <matthi.kuehne@gmail.com>
2026-02-03 09:24:06 +0100

    Button + / -

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-// MATTHIAS – nur lokaler Effekt im eigenen Bereich
+// MATTHIAS – NUR LOKALER EFFEKT IM EIGENEN BEREICH
 const btn = document.getElementById("matthiasBtn");
+const backbtn = document.getElementById("matthiasBackBtn");
 const out = document.getElementById("matthiasOutput");
 
+let count = 0;
+
 btn.addEventListener("click", () => {
-  out.textContent = "Button wurde geklickt 👍";
-});
+  count++;
+  out.textContent = "Button wurde " + count + " mal geklickt 👍";
+  }
+);
+
+backbtn.addEventListener("click", () => {
+  count--;
+    out.textContent = "Button wurde " + count + " mal geklickt 👍";
+  }
+);
