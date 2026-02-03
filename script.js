26559934ee499f0c8b75f9eb670b04676021ca69
Matthias Kühne <matthi.kuehne@gmail.com>
2026-02-03 11:42:33 +0100

    Zähler reset Button, Zähler anpassungen, w3schools link

--- script.js
+++ script.js
@@ -4,22 +4,34 @@ const backbtn = document.getElementById("matthiasBackBtn");
 const out = document.getElementById("matthiasOutput");
 const timeBtn = document.getElementById("matthiasTimeBtn");
 const timeOut = document.getElementById("matthiasTimeOutput");
+const resetBtn = document.getElementById("resetBtn");
 
 let count = 0;
 
 btn.addEventListener("click", () => {
   count++;
-  out.textContent = "Button wurde " + count + " mal geklickt 👍";
+  if (count === 0) {
+    out.textContent = "Back to zero";
+  } else {
+    out.textContent = "Button wurde " + count + " mal geklickt";
   }
-);
+});
 
 backbtn.addEventListener("click", () => {
   count--;
-    out.textContent = "Button wurde " + count + " mal geklickt 👍";
+  if (count === 0) {
+    out.textContent = "Back to zero";
+  } else {
+    out.textContent = "Button wurde " + count + " mal geklickt";
   }
-);
+});
 
 timeBtn.addEventListener("click", () => {
   const now = new Date();
   timeOut.textContent = "Aktuelle Uhrzeit: " + now.toLocaleTimeString();
+});
+
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  out.textContent = "Reset Button wurde geklickt";
 });
