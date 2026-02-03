152113f70ad199c10afbd97c8229228932084830
Matthias Kühne <matthi.kuehne@gmail.com>
2026-02-03 11:03:01 +0100

    JS Bild + CSS, Zeit anzeigen Button + JS

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const btn = document.getElementById("matthiasBtn");
 const backbtn = document.getElementById("matthiasBackBtn");
 const out = document.getElementById("matthiasOutput");
+const timeBtn = document.getElementById("matthiasTimeBtn");
+const timeOut = document.getElementById("matthiasTimeOutput");
 
 let count = 0;
 
@@ -16,3 +18,8 @@ backbtn.addEventListener("click", () => {
     out.textContent = "Button wurde " + count + " mal geklickt 👍";
   }
 );
+
+timeBtn.addEventListener("click", () => {
+  const now = new Date();
+  timeOut.textContent = "Aktuelle Uhrzeit: " + now.toLocaleTimeString();
+});
